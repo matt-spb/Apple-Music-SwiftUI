87a74dc5f7cda33c41f9b8be238f6cbdb46d8fb7
Matvey Fedyshin <iosdevelopermatt@gmail.com>
2022-05-06 22:28:44 +0300

    Initial Commit

---
+++ AppleMusic/AppleMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusicApp.swift
+//  AppleMusic
+//
+//  Created by Матвей Федышин on 06.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppleMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
